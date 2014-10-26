7401b66d031d58e2b581e6ecd7a362e38898711f
James Candan <jcandan82@gmail.com>
2014-10-25 22:50:21 -0400

    added some clarification comments.

--- js/critter-jump.js
+++ js/critter-jump.js
@@ -3,7 +3,13 @@ domready(function()
     init();
 });
 
+// list variables
 var game;
+
+/**
+ * init game
+ * this will be the main function that initializes the game
+ */
 function init() {
 
     game = new GAME.CJEngine();
