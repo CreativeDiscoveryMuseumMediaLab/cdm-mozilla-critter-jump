e45aa6cdcdb07ef31c1925c869391e1e0a6d6066
Brent Carroll <the.ironman.mark1@live.com>
2015-01-10 16:14:10 +0000

    adding changes recomminged

---
+++ js/CutScene.js
@@ -0,0 +1,14 @@
+    //Emporer Scorpion Video
+    var Scorpions = $('<div>').html('<iframe src="https://vimeo.com/112414428"></iframe>');
+    
+var Critterer = {};
+
+//title screen
+Critterer.CutScene = function(){};
+
+Critterer.CutScene.prototype = {
+  create: function() {}, 
+  update: function() {
+    
+  },
+}; 
---
+++ js/Game.js
@@ -0,0 +1,11 @@
+var Critterer = {};
+
+//title screen
+Critterer.Game = function(){};
+
+Critterer.Game.prototype = {
+  create: function() {}, 
+  update: function() {
+    
+  },
+}; 
