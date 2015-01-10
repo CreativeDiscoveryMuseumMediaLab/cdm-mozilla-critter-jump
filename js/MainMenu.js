eec4e5e2ee157db3563186084605fcaa06ba9627
James Candan <jcandan82@gmail.com>
2015-01-10 16:03:57 -0500

    included some method comments.

--- js/MainMenu.js
+++ js/MainMenu.js
@@ -15,12 +15,20 @@ Critterer.MainMenu.prototype = {
         preload: function() {
             //this.critterer_logo = null;
         },
+
+        /**
+         * Implements create()
+         */
         create: function() {
 
             //this.critterer_logo = this.add.sprite(this.world.centerX, this.world.centerY - 75, 'critterer_logo');
             //this.critterer_logo.anchor.setTo(0.5, 0.5);
 
         },
+
+        /**
+         * Implements update()
+         */
         update: function() {
                 
             if(this.game.input.activePointer.justPressed()) {
