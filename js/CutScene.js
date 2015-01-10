99c06248128f697d7b3186d7e92375e752ae5594
James Candan <jcandan82@gmail.com>
2015-01-10 12:20:09 -0500

    code indendation adjustments.

--- js/CutScene.js
+++ js/CutScene.js
@@ -4,12 +4,12 @@ var Critterer = {};
 Critterer.CutScene = function(game){};
 
 Critterer.CutScene.prototype = {
+
   create: function() {
     //Emporer Scorpion Video
     //var Scorpions = $('<div>').html('<iframe src="https://vimeo.com/112414428"></iframe>');
   },
 
-update: function() {
-    
-  },
+  update: function() {}
+
 }; 
