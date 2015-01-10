41bd13a94aaebda1b62de8ec873d44f4acc2dc0b
James Candan <jcandan82@gmail.com>
2015-01-10 12:01:30 -0500

    move scorpion code to within the create method in CutScene and commented out for now.

--- js/CutScene.js
+++ js/CutScene.js
@@ -1,14 +1,15 @@
-    //Emporer Scorpion Video
-    var Scorpions = $('<div>').html('<iframe src="https://vimeo.com/112414428"></iframe>');
-    
 var Critterer = {};
 
 //title screen
 Critterer.CutScene = function(){};
 
 Critterer.CutScene.prototype = {
-  create: function() {}, 
-  update: function() {
+  create: function() {
+    //Emporer Scorpion Video
+    //var Scorpions = $('<div>').html('<iframe src="https://vimeo.com/112414428"></iframe>');
+  },
+
+update: function() {
     
   },
 }; 
