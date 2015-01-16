4e7f6ad6d69c6fc1340a536fb229b61bbe27c2d5
Brent Carroll <the.ironman.mark1@live.com>
2015-01-16 14:19:08 +0000

    Revert "finished with cutscence"
    This reverts commit 582da30be54b442ddd67b09dad96067eb725a3b5.

--- js/CutScene.js
+++ js/CutScene.js
@@ -6,34 +6,10 @@ Critterer.CutScene = function(game){};
 Critterer.CutScene.prototype = {
 
   create: function() {
-    
-
     //Emporer Scorpion Video
-    var Scorpions = <a class="fancybox" rel="group" href="https://vimeo.com/112414428&api=1&player_id=video1"><img src="https://vimeo.com/112414428&api=1&player_id=video1" alt="" /></a>;
-    
-    var Walking_Stick = <a class="fancybox" rel="group" href="https://vimeo.com/111256268&api=1&player_id=video2"><img src="https://vimeo.com/111256268&api=1&player_id=video2" alt="" /></a>;
-    
-    var Walking_Stick_Breathe = <a class="fancybox" rel="group" href="https://vimeo.com/111256267&player_id=video3"><img src="https://vimeo.com/111256267&player_id=video3" alt="" /></a>;&api=1&player_id=video1
-    
-    var Walking_Stick_do = <a class="fancybox" rel="group" href="https://vimeo.com/111256266&api=1&player_id=video4"><img src="https://vimeo.com/111256266&api=1&player_id=video4" alt="" /></a>;
-    
-    var Walking_Sticks_live = <a class="fancybox" rel="group" href="https://vimeo.com/111256265&api=1&player_id=video1"><img src="https://vimeo.com/111256265&api=1&player_id=video1" alt="" /></a>;
+    //var Scorpions = $('<div>').html('<iframe src="https://vimeo.com/112414428"></iframe>');
   },
-  
 
   update: function() {}
-  
-      function test() {
-        var values = [Scorpions,Walking_Stick,Walking_Stick_Breathe,Walking_Stick_do, Walking_Sticks_live],
-            valueToUse = values[Math.floor(Math.random() * values.length)];
-        // do something with the selected value
-        $(document).ready(function() {
-		$(valueToUse).fancybox();
-    	});
-    }
+
 }; 
-
-
-	$(document).ready(function() {
-		$(".fancybox").fancybox();
-	});
