78de673d126c7a2f3be6c88560209267223f0f8c
Carter Smith <buckeyefan007@comcast.net>
2015-01-31 15:51:59 +0000

    testing

---
+++ js/GameOver.js
@@ -0,0 +1,11 @@
+// js/GameOver.js
+Critterer.GameOver = function(game){};
+
+Critterer.GameOver.prototype = {
+  preloader: function() {
+
+  },
+  create: function() {
+  },
+  update: function() {}
+}; 
