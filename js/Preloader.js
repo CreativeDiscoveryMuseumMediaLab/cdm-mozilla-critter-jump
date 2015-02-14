66793b74b9cf6246a029dd3a2b9901fdd5f760f2
Brent Carroll <the.ironman.mark1@live.com>
2015-02-14 20:27:34 +0000

    adding a comment to preloader

--- js/Preloader.js
+++ js/Preloader.js
@@ -8,6 +8,7 @@ Critterer.Preloader = function(game) {
     this.loader_lbl = null;
     this.game_play_btn = null;
     this.ready = false;
+    console.log("test")
 };
 
 Critterer.Preloader.prototype = {
